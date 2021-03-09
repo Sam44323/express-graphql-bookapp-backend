940e036f1776b607c9617407d5f37c2565257e29
Suranjan <suranjanbiswas2002@gmail.com>
2021-03-09 21:33:04 +0530

    resolver function completed and graphqlid added to the id type

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,28 @@
-const { GraphQLObjectType, GraphQLString } = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+} = require('graphql');
+const _ = require('lodash');
+
+let books = [
+  {
+    name: 'Name of Wind',
+    genre: 'Fantasy',
+    id: '1',
+  },
+  {
+    name: 'The Final Empire',
+    genre: 'Fantasy',
+    id: '2',
+  },
+  {
+    name: 'The Long Earth',
+    genre: 'Sci-Fi',
+    id: '3',
+  },
+];
 
 //defining the object types for the schemas
 
@@ -6,7 +30,7 @@ const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
     id: {
-      type: GraphQLString,
+      type: GraphQLID,
     },
     name: {
       type: GraphQLString,
@@ -15,4 +39,26 @@ const BookType = new GraphQLObjectType({
       type: GraphQLString,
     },
   }),
+});
+
+//defining a root query for the graphql schema
+
+/* For example, when we will start a query with book, then this will go to the RootQuery and find the schema with the query related to the book query in the field */
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        //code to get data from db / other source
+        return _.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const bookSchema = require('./schema/schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  
-}));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: bookSchema,
+    graphiql: true,
+  })
+);
 
 app.get('/', (req, res) => res.json('This is working!'));
 
